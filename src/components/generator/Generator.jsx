350655076ce616c2d870c1626f74be1c78f1ef8f
Santiago Bedoya <miguelsant2001@gmail.com>
2022-10-10 23:32:52 +0000

    Update Generator.jsx

--- src/components/generator/Generator.jsx
+++ src/components/generator/Generator.jsx
@@ -1,7 +1,8 @@
-import styled, { ThemeConsumer } from "styled-components";
-import { SketchPicker } from "react-color";
+import style from "styled-components";
 import { useState } from "react";
-import { Horizontal, Vertical } from "../cards/Cards";
+import DragDrop from "../drag-drop/DragDrop";
+import SpaceWorker from "../space-worker/SpaceWorker";
+import Code from "../code/Code";
 const Generator = () => {
   const [color, setColor] = useState("#fff");
   const Background = styled.div`

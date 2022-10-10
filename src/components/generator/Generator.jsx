06a61e0e9cdb7b42e268ddbdfc47fb78ffaba5cb
Santiago Bedoya <miguelsant2001@gmail.com>
2022-10-10 23:35:50 +0000

    Update Generator.jsx

--- src/components/generator/Generator.jsx
+++ src/components/generator/Generator.jsx
@@ -1,4 +1,4 @@
-import styled, { ThemeConsumer } from "styled-components";
+import styled from "styled-components";
 
 import { useState } from "react";
 import DragDrop from "../drag-drop/DragDrop";

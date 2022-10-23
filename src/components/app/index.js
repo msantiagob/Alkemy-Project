4e777de818034c005dfd02bed3985f4102b8f613
Santiago Bedoya <miguelsant2001@gmail.com>
2022-10-23 17:15:36 +0000

    Update index.js and Header.jsx update path lauta at card-animada

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,39 +2,43 @@ import "./style.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Generator from "../generator/Generator";
 import { Home } from "../Home/Home";
-import { Login } from "../auth/Login/Login"
-import { Register } from "../auth/Register/Register"
+import { Login } from "../auth/Login/Login";
+import { Register } from "../auth/Register/Register";
 import Lauta from "../Animations/Examples";
 import { ColorProvider } from "../../context/ColorContext";
-import { Header} from "../header/Header"
+import { Header } from "../header/Header";
 
 function App() {
-
-
-  const RequireAuth = ({children}) =>{
-    if (!localStorage.getItem('logged')){
-        return <Navigate to='/login' replace={true} />
+  const RequireAuth = ({ children }) => {
+    if (!localStorage.getItem("logged")) {
+      return <Navigate to="/login" replace={true} />;
     }
-    return children
-}  
+    return children;
+  };
   return (
     <ColorProvider>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={
-            <RequireAuth>
-              <Generator />
-            </RequireAuth>
-            }/>
-          <Route path='lauta' element={
-            <RequireAuth>
-              <Lauta/>
-            </RequireAuth>
-          }/>
-          <Route path='/home' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <Generator />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="card-animada"
+            element={
+              <RequireAuth>
+                <Lauta />
+              </RequireAuth>
+            }
+          />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </ColorProvider>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,18 @@
-import './header.style.css'
-import { Link } from 'react-router-dom'
+import "./header.style.css";
+import { Link } from "react-router-dom";
 
-export const Header = ()=> {
-    return(
-        <nav className="navbar">
-            <span>Hola</span> 
-            <ul className='navbar--list'>
-                <li>
-                    <Link to='/'>Card</Link>
-                </li>
-                <li>
-                    <Link to='/lauta'>Card animada</Link>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+export const Header = () => {
+  return (
+    <nav className="navbar">
+      <span>Hola</span>
+      <ul className="navbar--list">
+        <li>
+          <Link to="/">Card</Link>
+        </li>
+        <li>
+          <Link to="/card-animada">Card animada</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
